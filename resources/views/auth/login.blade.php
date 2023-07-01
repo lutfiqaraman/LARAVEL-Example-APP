c2498148dcac47687c79ad182801bd7e890ac2fd
lutfiqaraman <lutfiqaraman@gmail.com>
2023-07-01 17:20:19 +0300

    Building login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,81 +3,82 @@
 @section('content')
 
     <div class="grid grid-cols-2">
-        <div>
-            <section class="bg-gray-50 dark:bg-gray-900">
-                <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <section>
+            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="p-6 space-y-4 md:space-y-6 sm:p-4">
 
-                    <a href="#" class="flex items-center mb-4 text-2xl font-semibold text-gray-900 dark:text-white">
-                        EXAMPLE APP
-                    </a>
+                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Welcome back
+                        </h1>
 
-                    <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                        <div class="p-6 space-y-4 md:space-y-6 sm:p-4">
+                        <div class="flow-root">
 
-                            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Welcome back
-                            </h1>
+                            <a class="border border-solid border-gray-200 rounded-lg px-5 py-2 float-left hover:bg-gray-200" href="#">
+                                <span><i class="fa-brands fa-google"></i></span> Log in with Google
+                            </a>
 
-                            <div class="flow-root">
-
-                                <a class="border border-solid border-gray-200 rounded-lg px-5 py-2 float-left hover:bg-gray-200" href="#">
-                                    <span><i class="fa-brands fa-google"></i></span> Log in with Google
-                                </a>
-
-                                <a class="border border-solid border-gray-200 rounded-lg px-5 py-2 float-right hover:bg-gray-200" href="#">
-                                    <span><i class="fa-brands fa-apple"></i></span> Log in with Apple</a>
-                            </div>
-
-                            <div class="relative flex py-1 items-center">
-                                <div class="flex-grow border-t border-gray-400"></div>
-                                <span class="flex-shrink mx-4 text-gray-400">or</span>
-                                <div class="flex-grow border-t border-gray-400"></div>
-                            </div>
-
-                            <form class="space-y-8 md:space-y-3" action="{{ route('authenticate') }}" method="post">
-                                <div>
-                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('email') is-invalid @enderror"  value="{{ old('email') }}" placeholder="name@company.com" required="" >
-                                    @if ($errors->has('email'))
-                                        <span class="text-danger">{{ $errors->first('email') }}</span>
-                                    @endif
-                                </div>
-                                <div>
-                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('password') is-invalid @enderror" required="">
-                                    @if ($errors->has('password'))
-                                        <span class="text-danger">{{ $errors->first('password') }}</span>
-                                    @endif
-                                </div>
-                                <div class="flex items-center justify-between">
-                                    <div class="flex items-start">
-                                        <div class="flex items-center h-5">
-                                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
-                                        </div>
-                                        <div class="ml-3 text-sm">
-                                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                                        </div>
-                                    </div>
-                                    <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Forgot password?</a>
-                                </div>
-
-                                <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in
-                                </button>
-
-                                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Don’t have an account yet? <a href="#" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Sign up</a>
-                                </p>
-
-                            </form>
+                            <a class="border border-solid border-gray-200 rounded-lg px-5 py-2 float-right hover:bg-gray-200" href="#">
+                                <span><i class="fa-brands fa-apple"></i></span> Log in with Apple</a>
                         </div>
-                    </div>
 
+                        <div class="relative flex py-1 items-center">
+                            <div class="flex-grow border-t border-gray-400"></div>
+                            <span class="flex-shrink mx-4 text-gray-400">or</span>
+                            <div class="flex-grow border-t border-gray-400"></div>
+                        </div>
+
+                        <form class="space-y-8 md:space-y-3" action="{{ route('authenticate') }}" method="post">
+                            <div>
+                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('email') is-invalid @enderror"  value="{{ old('email') }}" placeholder="name@company.com" required="" >
+                                @if ($errors->has('email'))
+                                    <span class="text-danger">{{ $errors->first('email') }}</span>
+                                @endif
+                            </div>
+                            <div>
+                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('password') is-invalid @enderror" required="">
+                                @if ($errors->has('password'))
+                                    <span class="text-danger">{{ $errors->first('password') }}</span>
+                                @endif
+                            </div>
+                            <div class="flex items-center justify-between">
+                                <div class="flex items-start">
+                                    <div class="flex items-center h-5">
+                                        <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                                    </div>
+                                    <div class="ml-3 text-sm">
+                                        <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                                    </div>
+                                </div>
+                                <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Forgot password?</a>
+                            </div>
+
+                            <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in
+                            </button>
+
+                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                                Don’t have an account yet? <a href="#" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Sign up</a>
+                            </p>
+
+                        </form>
+                    </div>
                 </div>
-            </section>
-        </div>
-        <div class="h-100 bg-blue-700">
-            <p class="text-white">description is here</p>
-        </div>
+            </div>
+        </section>
+        <section>
+            <div
+                class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 h-100 bg-gray-100">
+                <a href="#" class="flex items-center mb-4 text-2xl font-semibold text-gray-900 dark:text-white">
+                    EXAMPLE APP
+                </a>
+                <p class="text-gray-900 text-center">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia, aspernatur itaque ratione
+                    repellendus molestiae quibusdam cum nobis!
+                </p>
+            </div>
+        </section>
     </div>
 
 @endsection
